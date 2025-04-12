f175ad39334436e90a297134b826df4f84838e83
Raffort <rafaelmenegalli@gmail.com>
2025-04-11 22:35:50 -0300

    fic

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  transpilePackages: ['rsuite/esm/locales'],
+  experimental: {
+    esmExternals: 'loose',
+  },
 };
 
 export default nextConfig;
